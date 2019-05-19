fa803496900b46d08b15630bc8f3032623492ff2
UCLAShawn3G <uclashawn1@gmail.com>
2019-05-18 18:16:04 -0700

    profile created with interests-route and model

--- models/Profile.js
+++ models/Profile.js
@@ -5,105 +5,45 @@ const ProfileSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "user"
   },
-  company: {
-    type: String
-  },
-  website: {
+  name: {
     type: String
   },
   location: {
-    type: String
+    type: String,
+    required: true
   },
   status: {
     type: String,
     required: true
   },
-  skills: {
-    type: [String],
-    required: true
-  },
-  bio: {
-    type: String
-  },
-  githubusername: {
-    type: String
-  },
-  experience: [
+  interests: [
     {
-      title: {
-        type: String,
-        required: true
-      },
-      company: {
-        type: String,
-        required: true
-      },
-      location: {
-        type: String
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
+      household: {
         type: Boolean,
         default: false
       },
-      description: {
-        type: String
-      }
-    }
-  ],
-  education: [
-    {
-      school: {
-        type: String,
-        required: true
-      },
-      degree: {
-        type: String,
-        required: true
-      },
-      fieldofstudy: {
-        type: String,
-        required: true
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
+      events: {
         type: Boolean,
         default: false
       },
-      description: {
-        type: String
+      automotive: {
+        type: Boolean,
+        default: false
+      },
+      health: {
+        type: Boolean,
+        default: false
+      },
+      educational: {
+        type: Boolean,
+        default: false
+      },
+      groceries: {
+        type: Boolean,
+        default: false
       }
     }
   ],
-  social: {
-    youtube: {
-      type: String
-    },
-    twitter: {
-      type: String
-    },
-    facebook: {
-      type: String
-    },
-    linkedin: {
-      type: String
-    },
-    instagram: {
-      type: String
-    }
-  },
   date: {
     type: Date,
     default: Date.now
