e72afffc65230989f4d29fa2ab6d0060b0c07e90
UCLAShawn3G <uclashawn1@gmail.com>
2019-05-18 21:22:44 -0700

    interests.js

--- client/src/components/dashboard/Interests.js
+++ client/src/components/dashboard/Interests.js
@@ -1,25 +1,25 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import Moment from 'react-moment';
-import { connect } from 'react-redux';
-import { deleteInterests } from '../../actions/profile';
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+import { connect } from "react-redux";
+import { deleteInterests } from "../../actions/profile";
 
-const Interests = ({ Interests, deleteInterests }) => {
-  const Interestss = Interests.map(exp => (
-    <tr key={exp._id}>
-      <td>{exp.company}</td>
-      <td className='hide-sm'>{exp.title}</td>
+const Category = ({ category, deleteInterests }) => {
+  const interests = category.map(cat => (
+    <tr key={cat._id}>
+      <td>{cat.company}</td>
+      <td className='hide-sm'>{cat.title}</td>
       <td>
-        <Moment format='YYYY/MM/DD'>{exp.from}</Moment> -{' '}
-        {exp.to === null ? (
-          ' Now'
+        <Moment format='YYYY/MM/DD'>{cat.from}</Moment> -{" "}
+        {cat.to === null ? (
+          " Now"
         ) : (
-          <Moment format='YYYY/MM/DD'>{exp.to}</Moment>
+          <Moment format='YYYY/MM/DD'>{cat.to}</Moment>
         )}
       </td>
       <td>
         <button
-          onClick={() => deleteInterests(exp._id)}
+          onClick={() => deleteInterests(cat._id)}
           className='btn btn-danger'
         >
           Delete
@@ -30,7 +30,7 @@ const Interests = ({ Interests, deleteInterests }) => {
 
   return (
     <Fragment>
-      <h2 className='my-2'>Interests Credentials</h2>
+      <h2 className='my-2'>Experience Credentials</h2>
       <table className='table'>
         <thead>
           <tr>
@@ -40,18 +40,18 @@ const Interests = ({ Interests, deleteInterests }) => {
             <th />
           </tr>
         </thead>
-        <tbody>{Interestss}</tbody>
+        <tbody>{interests}</tbody>
       </table>
     </Fragment>
   );
 };
 
-Interests.propTypes = {
-  Interests: PropTypes.array.isRequired,
+Category.propTypes = {
+  interests: PropTypes.array.isRequired,
   deleteInterests: PropTypes.func.isRequired
 };
 
 export default connect(
   null,
   { deleteInterests }
-)(Interests);
+)(Category);
